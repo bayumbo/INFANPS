c641152dec6255dcd2ed5d7372b1353024ddba57
Eerick611 <erick199335@gmail.com>
2024-01-30 23:07:34 -0500

    cambios gestion contenido

--- src/controllers/gestionContenidosController.js
+++ src/controllers/gestionContenidosController.js
@@ -1,8 +1,8 @@
-const { GestionContenidos, Usuarios } = require('../models/gestionContenidos');
+const { GestionContenido, Usuarios } = require('../Database/dataBase.orm');
 
 const obtenerGestionContenidos = async(req, res) => {
     try {
-        const gestionContenidos = await GestionContenidos.findAll();
+        const gestionContenidos = await GestionContenido.findAll();
         return res.json(gestionContenidos);
     } catch (error) {
         console.error(error);
@@ -12,7 +12,7 @@ const obtenerGestionContenidos = async(req, res) => {
 
 const crearGestionContenido = async(req, res) => {
     try {
-        const nuevoGestionContenido = await GestionContenidos.create(req.body);
+        const nuevoGestionContenido = await GestionContenido.create(req.body);
         return res.json(nuevoGestionContenido);
     } catch (error) {
         console.error(error);
@@ -23,7 +23,7 @@ const crearGestionContenido = async(req, res) => {
 const obtenerGestionContenidoPorId = async(req, res) => {
     const { id } = req.params;
     try {
-        const gestionContenido = await GestionContenidos.findByPk(id);
+        const gestionContenido = await GestionContenido.findByPk(id);
         if (!gestionContenido) {
             return res.status(404).json({ mensaje: 'Gestión de contenido no encontrada' });
         }
@@ -37,21 +37,24 @@ const obtenerGestionContenidoPorId = async(req, res) => {
 const actualizarGestionContenido = async(req, res) => {
     const { id } = req.params;
     try {
-        const [filasActualizadas, [gestionContenidoActualizado]] = await GestionContenidos.update(req.body, { where: { id }, returning: true });
+        const [filasActualizadas, gestionContenidoActualizados] = await GestionContenido.update(req.body, { where: { id }, returning: true });
+
         if (filasActualizadas === 0) {
-            return res.status(404).json({ mensaje: 'Gestión de contenido no encontrada' });
+            return res.status(404).json({ mensaje: 'Gestion de contenido no encontrado' });
         }
+
+        const gestionContenidoActualizado = gestionContenidoActualizados[0];
         return res.json(gestionContenidoActualizado);
     } catch (error) {
         console.error(error);
-        return res.status(500).json({ mensaje: 'Error al actualizar gestión de contenido' });
+        return res.status(500).json({ mensaje: 'Error al actualizar perfil'  });    
     }
 };
 
 const eliminarGestionContenido = async(req, res) => {
     const { id } = req.params;
     try {
-        const filasEliminadas = await GestionContenidos.destroy({ where: { id } });
+        const filasEliminadas = await GestionContenido.destroy({ where: { id } });
         if (filasEliminadas === 0) {
             return res.status(404).json({ mensaje: 'Gestión de contenido no encontrada' });
         }
